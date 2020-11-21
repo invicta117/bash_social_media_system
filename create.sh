59d9b49f309ff24b530380862d09f3505bf0b06f
Killian Cooney <killian.cooney@ucdconnect.ie>
2020-11-21 21:09:33 +0000

    trying again

--- create.sh
+++ create.sh
@@ -3,11 +3,11 @@ if [ $# -ne 1 ]; then
 	echo "Error: parameters problem"
 	exit 1	
 fi
-
-if mkdir "$1" 2>/dev/null; then
-	echo "OK: user created"
-else
-	echo "Error: user already exists"
-fi 
+mkdir $1
+#if mkdir "$1" 2>/dev/null; then
+#	echo "OK: user created"
+#else
+#	echo "Error: user already exists"
+#fi 
 
 exit 1
