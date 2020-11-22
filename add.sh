ad28a8766ef6e0907321d714f58a6138a96d7dec
Killian Cooney <killian.cooney@ucdconnect.ie>
2020-11-22 15:13:47 +0000

    testing if friends and user exist

--- add.sh
+++ add.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+if [ $# -ne 2 ]; then
+	echo "Error: parameters problem" >&2
+	exit 1
+fi
+
+if [ ! -d "$1" ]; then
+	echo "Error: user does not exist" >&2
+	exit 1
+elif [ ! -d "$2" ]; then
+	echo "Error: friend does not exist" >&2
+	exit 1
+fi
+
+
+exit 0
+
