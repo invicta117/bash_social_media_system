35c9061bacb50284408b98ad4b0d6b00f390c2a5
Killian Cooney <killian.cooney@ucdconnect.ie>
2020-11-22 15:01:52 +0000

    adding the friends and wall files

--- create.sh
+++ create.sh
@@ -3,11 +3,19 @@ if [ $# -ne 1 ]; then
 	echo "Error: parameters problem"
 	exit 1	
 fi
-mkdir $1
-#if mkdir "$1" 2>/dev/null; then
-#	echo "OK: user created"
+
+#if [ $(dirname $1) == "." ]; then
+#	echo "we have a new directory"
 #else
-#	echo "Error: user already exists"
-#fi 
+#	echo "we have a new file"
+#fi
+
+if mkdir "$1" 2>/dev/null; then
+	touch "./$1/friends"
+	touch "./$1/wall"
+	echo "OK: user created"
+else
+	echo "Error: user already exists"
+fi 
 
 exit 1
