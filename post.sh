2ad352830496e4621b9ea47dacd27a564928a047
Killian Cooney <killian.cooney@ucdconnect.ie>
2020-11-27 23:10:02 +0000

    internet outage attempt

--- post.sh
+++ post.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+if [ $# -ne 3 ]; then
+        echo "Error: paramater problem"
+        exit 1 # I think it is non zero
+fi
+
+if ! [ -d $1 ]; then
+        echo "Error: Receiver does not exist"
+        exit 1
+fi
+
+if ! [ -d $2 ]; then
+        echo "Error: Sender does not exist"
+        exit 1
+fi
+
+if ! grep -q $2 ./$1/friends; then
+        echo "Error: Sender is not a friend of receiver"
+        exit 1
+fi
+
+echo "$2: $3" >> "./$1/wall"
+echo "Ok: message posted to wall"
+exit 0
+
--- show.sh
+++ show.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo "Error: paramaters problem"
+	exit 1
+fi
+
+if ! [ -d $1 ]; then
+	echo "Error: user does not exist"
+	exit 1
+fi
+
+wall="$1/wall"
+num=$(wc -l $wall | cut -d " " -f1)
+echo "WallStart"
+head -n$num $wall
+echo "WallEnd"
+exit 0
